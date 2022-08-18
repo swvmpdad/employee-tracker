50e25145a6d38445c5baab9b7e697fc8627ef605
swvmpdad <will.g.camp@gmail.com>
2022-08-18 18:32:10 -0500

    added main menu function

--- app.js
+++ app.js
@@ -1,1 +1,68 @@
 const inquirer = require('inquirer');
+
+function mainMenu() {
+    console.log(`
+██████████████████████████████████████████████████
+█▄─▄▄─█▄─▀█▀─▄█▄─▄▄─█▄─▄███─▄▄─█▄─█─▄█▄─▄▄─█▄─▄▄─█
+██─▄█▀██─█▄█─███─▄▄▄██─██▀█─██─██▄─▄███─▄█▀██─▄█▀█
+▀▄▄▄▄▄▀▄▄▄▀▄▄▄▀▄▄▄▀▀▀▄▄▄▄▄▀▄▄▄▄▀▀▄▄▄▀▀▄▄▄▄▄▀▄▄▄▄▄▀
+███████████████████████████████████████████
+█─▄─▄─█▄─▄▄▀██▀▄─██─▄▄▄─█▄─█─▄█▄─▄▄─█▄─▄▄▀█
+███─████─▄─▄██─▀─██─███▀██─▄▀███─▄█▀██─▄─▄█
+▀▀▄▄▄▀▀▄▄▀▄▄▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▀▄▄▀
+    `);
+    return inquirer
+    .prompt([
+        {
+        type: 'list',
+        name: 'menu',
+        message: 'What would you llike to do?',
+        choices: [
+            'View All Employees',
+            'Add Employee',
+            'View Departments',
+            'Add Department',
+            'View Managers',
+            'Add Manager',
+            'View All Roles',
+            'Add Role',
+            'Update Employee Role',
+            'Quit'
+        ]
+        }
+    ])
+    .then(response => {
+        if (response.menu === 'View All Employees') {
+
+        }
+        else if (response.menu === 'Add Employee') {
+
+        }
+        else if (response.menu === 'View Departments') {
+            
+        }
+        else if (response.menu === 'Add Department') {
+            
+        }
+        else if (response.menu === 'View Managers') {
+            
+        }
+        else if (response.menu === 'View All Roles') {
+            
+        }
+        else if (response.menu === 'Add Role') {
+            
+        }
+        else if (response.menu === 'Update Employee Role') {
+            
+        }
+        else if (response.menu === 'Add Manager') {
+            
+        }
+        else if (response.menu === 'Quit') {
+
+        }
+    });
+}
+
+mainMenu();
