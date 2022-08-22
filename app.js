21faeeca44d25a6eff9f2100d945815e6f47ce79
swvmpdad <will.g.camp@gmail.com>
2022-08-22 18:11:30 -0500

    added comments

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+// allows us to use inquirer and connect to the sql database
 const inquirer = require('inquirer');
 const db = require('./db/connection');
 
 function viewEmployees() {
+    // joining the tables to show specific information
+    // about each employee
     db.query(`
     SELECT employee.id AS ID,
     employee.first_name AS First,
@@ -15,10 +18,10 @@ function viewEmployees() {
     LEFT JOIN role
     ON employee.role_id = role.id;
     `, function(err, res) {
-        if (err) {
-            console.log(err);
-        }
+        if (err) throw err;
+        // displaying the called info to a table
         console.table(res);
+        // calling main menu function
         mainMenu();
     });
 }
@@ -61,6 +64,9 @@ function addEmployee() {
                 choices: function() {
                     let choicesArr = [];
                     for (var i = 0; i < res.length; i++) {
+                        // The results of the db.query UNION combine the two tables
+                        // so I pulled the choices based off of the salary 
+                        // being a number and not a last name from the manager table
                         if (isNaN(res[i].salary) === false) {
                         choicesArr.push(res[i].title);
                         }
@@ -75,6 +81,9 @@ function addEmployee() {
                 choices: function() {
                     let choicesArr2 = [];
                     for (var i = 0; i < res.length; i++) {
+                        // I did the opposite here and if it returned a
+                        // number it would skip until it found a last 
+                        // name in the salary column
                         if (isNaN(res[i].salary) === true)
                         choicesArr2.push(res[i].title);
                     }
@@ -87,6 +96,7 @@ function addEmployee() {
             let lastName =  response.lastName;
             let roleId;
             let managerId;
+            // function to pull the role.id by matching it to the response.title
             function getRoleId() {
                 for (var i = 0; i < res.length; i++) {
                     if (res[i].title === response.roleId) {
@@ -95,6 +105,7 @@ function addEmployee() {
                     }
                 }
             }
+            // function to pull the manager.id by matching it to the response.title
             function getManagerId() {
                 for (var i = 0; i < res.length; i++) {
                     if (res[i].title === response.managerId) {
@@ -103,11 +114,14 @@ function addEmployee() {
                     }
                 }
             }
+            // calling both functions
             getRoleId();
             getManagerId();
+            // querying the db to get the next increment of employee id
             db.query("SELECT * FROM employee", (err, res) => {
                 if (err) throw err;
                 let employeeId = res.length + 1;
+                // inserting the new values into the employee table
                 db.query(
                     "INSERT INTO employee SET ?",
                     [{
@@ -123,6 +137,7 @@ function addEmployee() {
                         Employee added successfully!
                         ============================
                         `);
+                        // calling the main menu after the function has run
                         mainMenu()
                     })
             });
@@ -131,15 +146,16 @@ function addEmployee() {
 }
 
 function viewDepartments() {
+    // query to show departments
     db.query(`
     SELECT department.id AS ID,
     department.name AS Name
     FROM department;
     `, function(err, res) {
-        if (err) {
-            console.log(err);
-        }
+        if (err) throw err;
+        // displaying departments in a table
         console.table(res);
+        // calling the main menu
         mainMenu();
     })
 }
@@ -162,6 +178,7 @@ function addDepartment() {
         }
     ])
     .then(response => {
+        // inserting the new department into the department table
         db.query(
             "INSERT INTO department SET ?",
             [{
@@ -173,6 +190,7 @@ function addDepartment() {
                 Department added successfully!
                 ==============================
                 `);
+                // calling main menu
                 mainMenu()
             }
         )
@@ -180,6 +198,7 @@ function addDepartment() {
 }
 
 function viewManagers() {
+    // queries all managers
     db.query(`
     SELECT manager.id AS ID,
     manager.first_name AS First,
@@ -189,15 +208,16 @@ function viewManagers() {
     LEFT JOIN department
     ON manager.department_id = department.id;
     `, function(err, res) {
-        if (err) {
-            console.log(err);
-        }
+        if (err) throw err;
+        // displays all managers in a console table
         console.table(res);
+        // calls main menu
         mainMenu();
     });
 }
 
 function addManager() {
+    // queries the department table to generate department choices
     db.query("SELECT * FROM department", (err, res) => {
         if (err) throw err;
         return inquirer
@@ -234,6 +254,7 @@ function addManager() {
                 message: `What is the manager's department?`,
                 choices: function () {
                     let choicesArr = [];
+                    // returns an array of department choices
                     for (var i = 0; i < res.length; i++) {
                         choicesArr.push(res[i].name);
                     }
@@ -242,31 +263,41 @@ function addManager() {
             }
         ])
         .then(response => {
-            db.query(`SELECT * FROM department WHERE name = "${response.department}"`, (err, res) => {
-                if (err) throw err;
-                let departmentChoice = res[0].id;
-                db.query(
-                    "INSERT INTO manager SET ?",
-                    [{
-                        first_name: response.firstName,
-                        last_name: response.lastName,
-                        department_id: departmentChoice
-                    }], err => {
-                        if (err) throw err;
-                        console.log(`
-                        ============================
-                        Manager added successfully!
-                        ============================
-                        `);
-                        mainMenu()
+            // gets the department id from the initial query
+            let departmentId;
+            function getDeptId() {
+                for (var i = 0; i < res.length; i++) {
+                    if (res[i].name === response.department) {
+                        departmentId = res[i].id;
+                        return departmentId;
                     }
-                )
-            })
+                }
+            }
+            getDeptId();
+            // inserts data into the table
+            db.query(
+                "INSERT INTO manager SET ?",
+                [{
+                    first_name: response.firstName,
+                    last_name: response.lastName,
+                    department_id: departmentId
+                }], err => {
+                    if (err) throw err;
+                    console.log(`
+                    ============================
+                    Manager added successfully!
+                    ============================
+                    `);
+                    // calls the main menu
+                    mainMenu()
+                }
+            )
         })
     })
 }
 
 function viewRoles() {
+    // queries the role and department tables and joins them
     db.query(`
     SELECT role.title AS Title,
     role.salary AS Salary,
@@ -275,15 +306,16 @@ function viewRoles() {
     LEFT JOIN department
     ON role.department_id = department.id;
     `, function(err, res) {
-        if (err) {
-            console.log(err);
-        }
+        if (err) throw err;
+        // displays the table
         console.table(res);
+        // calls the main menu
         mainMenu();
     })
 }
 
 function addRole() {
+    // calls the department to generate dynamic choice list
     db.query("SELECT * FROM department", function(err, res) {
     if (err) throw err;
      return inquirer
@@ -306,6 +338,7 @@ function addRole() {
             name: 'salary',
             message: "What is the role's salary?",
             validate: salaryInput => {
+                // validates whether the input is a number or not
                 if (isNaN(salaryInput) === false){
                     return true;
                 } else {
@@ -320,6 +353,7 @@ function addRole() {
             message: `What is the role's department?`,
             choices: function() {
                 var choicesArr = [];
+                // generates a dynamic choice array
                 for (var i = 0; i < res.length; i++) {
                     choicesArr.push(res[i].name)
                 }
@@ -331,6 +365,7 @@ function addRole() {
         let title = response.title;
         let salary = response.salary;
         let departmentId;
+        // gets the department id from the original query
         function getDeptId() {
             for (var i = 0; i < res.length; i++) {
                 if (res[i].name === response.department) {
@@ -340,9 +375,11 @@ function addRole() {
             }
         }
         getDeptId();
+        // gets the next increment of the role id
         db.query(`SELECT * FROM role`, (err,res) => {
             if (err) throw err;
             let id = res.length + 1;
+            // inserts data into table
             db.query(
                 "INSERT INTO role SET ?",
                 [{
@@ -357,6 +394,7 @@ function addRole() {
                     Role added successfully!
                     ========================
                     `);
+                    // calls the main menu
                     mainMenu();
                 }
             )
@@ -366,6 +404,7 @@ function addRole() {
 }
 
 function updateRole() {
+    // calls a list of employees to generate choices
     db.query(`SELECT * FROM employee`, (err,res) => {
         if (err) throw err;
         inquirer
@@ -376,6 +415,7 @@ function updateRole() {
                     message: 'Which employee would you like to update?',
                     choices: function() {
                         let choicesArr = [];
+                        // generates a list of employee choices
                         for (var i = 0; i < res.length; i++) {
                             choicesArr.push(res[i].first_name);
                         }
@@ -384,8 +424,9 @@ function updateRole() {
                 }
             ])
             .then(response => {
+                // defines the chosen employee's first name for use later
                 const employeeName = response.employee;
-
+                // calls the roles to generate a list of choices
                 db.query(`SELECT * FROM role`, (err, res) => {
                     if (err) throw err;
                     inquirer
@@ -396,6 +437,7 @@ function updateRole() {
                                 message: `What is the ID of the employee's new role?`,
                                 choices: function() {
                                     let choicesArr = [];
+                                    // generates list of roles
                                     for (var i = 0; i < res.length; i++) {
                                         choicesArr.push(res[i].id);
                                     }
@@ -404,8 +446,9 @@ function updateRole() {
                             }
                         ])
                         .then(response => {
+                            // defines the chosen role
                             const roleChoice = response.role;
-        
+                            // inserts and overrides the current data in the table based off of the employeeName
                             db.query(`UPDATE employee SET ? WHERE first_name = ?`, 
                                 [
                                     {
@@ -418,6 +461,7 @@ function updateRole() {
                                     Role updated successfully!
                                     ==========================
                                     `);
+                                    // calls main menu
                                     mainMenu();
                                 }
                         )
@@ -427,7 +471,9 @@ function updateRole() {
     })
 }
 
+// The main menu that you'll see in the app
 function mainMenu() {
+    // connection to the sql database
     db.connect(function(err) {
         if (err) {
             console.log(err);
@@ -464,6 +510,7 @@ function mainMenu() {
         ]
         }
     ])
+    // calling the function based on the response of the user
     .then(response => {
         if (response.menu === 'View All Employees') {
             viewEmployees();
@@ -506,4 +553,5 @@ function mainMenu() {
     });
 }
 
+// starting the menu
 mainMenu();
