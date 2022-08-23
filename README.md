2e85e7410772f3a35d1100e0fa90ba6bd95f027d
swvmpdad <will.g.camp@gmail.com>
2022-08-22 21:17:35 -0500

    added remove functions

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# employee-tracker
+
+
+# Employee Tracker
+
+## Description
+
+A console application to maintain an employee database
+
+Made using MySql, Node.js, Javascript, and Inquirer
+
+
+## Installation
+
+- Navigate to the app's root directory 
+- Run 'npm i' to install the required Node packages 
+- Edit the .env.EXAMPLE file with the appropriate information 
+- Delete the .example from the env file 
+
+
+
+
+
+## Usage
+
+- Navigate to the root directory
+- Run 'node app'
+- Select an option and follow the prompts
+- To exit the application select the 'quit' option
+
+
+
+
+
+
+
+Made by William Campbell.
+Find me on GitHub @ [swvmpdad](https://github.com/swvmpdad)
--- app.js
+++ app.js
@@ -26,6 +26,105 @@ function viewEmployees() {
     });
 }
 
+function viewEmployeesByManager() {
+    db.query(`SELECT * FROM manager`, (err, res) => {
+        return inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'manager',
+                message: `Which manager's employees would you like to view?`,
+                choices: function() {
+                    let choicesArr = [];
+                    for (var i = 0; i < res.length; i++) {
+                        choicesArr.push(res[i].first_name);
+                    }
+                    return choicesArr;
+                }
+            }
+        ])
+          .then(response => {
+            let managerId;
+            function getManagerId() {
+                for (var i = 0; i < res.length; i++) {
+                    if (res[i].first_name === response.manager) {
+                        managerId = res[i].id;
+                        return managerId;
+                    }
+                }
+            }
+            getManagerId();
+            db.query(`SELECT employee.id AS ID,
+            employee.first_name AS First,
+            employee.last_name AS Last,
+            role.title AS Role,
+            role.salary AS Salary
+            FROM employee
+            LEFT JOIN role
+            ON employee.role_id = role.id
+            WHERE employee.manager_id = ${managerId}`,
+            function(err, res) {
+                if (err) throw err;
+                // displaying the called info to a table
+                console.table(res);
+                // calling main menu function
+                mainMenu();
+            })
+          })
+    })
+}
+
+function viewEmployeesByDepartment() {
+    db.query(`SELECT * FROM department`, (err, res) => {
+        return inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'department',
+                message: `Which department's employees would you like to view?`,
+                choices: function() {
+                    let choicesArr = [];
+                    for (var i = 0; i < res.length; i++) {
+                        choicesArr.push(res[i].name);
+                    }
+                    return choicesArr;
+                }
+            }
+        ])
+          .then(response => {
+            let departmentId;
+            function getDeptId() {
+                for (var i = 0; i < res.length; i++) {
+                    if (res[i].name === response.department) {
+                        departmentId = res[i].id;
+                        return departmentId;
+                    }
+                }
+            }
+            getDeptId();
+            db.query(`SELECT employee.id AS ID,
+            employee.first_name AS First,
+            employee.last_name AS Last,
+            role.title AS Role,
+            role.salary AS Salary,
+            manager.first_name AS Manager
+            FROM employee
+            LEFT JOIN manager
+            ON employee.manager_id = manager.id
+            LEFT JOIN role
+            ON employee.role_id = role.id
+            WHERE role.department_id = ${departmentId}`,
+            function(err, res) {
+                if (err) throw err;
+                // displaying the called info to a table
+                console.table(res);
+                // calling main menu function
+                mainMenu();
+            })
+          })
+    })
+}
+
 function addEmployee() {
     db.query("SELECT * FROM role UNION SELECT * FROM manager", (err,res) => {
         if (err) throw err;
@@ -471,6 +570,138 @@ function updateRole() {
     })
 }
 
+function removeEmployee() {
+    db.query(`SELECT * FROM employee`, (err, res) => {
+        if (err) throw err;
+        return inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'employee',
+                message: 'Which Employee would you like to remove?',
+                choices: function() {
+                    let choicesArr = [];
+                    for (var i = 0; i < res.length; i++) {
+                        choicesArr.push(res[i].first_name);
+                    }
+                    return choicesArr;
+                }
+            }
+        ])
+          .then(response => {
+            db.query(`DELETE FROM employee WHERE first_name = '${response.employee}'`,
+            (err, res) => {
+                if (err) throw err;
+                console.log(`
+                ==============================
+                Employee Removed Successfully!
+                ==============================
+                `);
+                mainMenu();
+            })
+          })
+    })
+}
+
+function removeDepartment() {
+    db.query(`SELECT * FROM department`, (err, res) => {
+        if (err) throw err;
+        return inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'department',
+                message: 'Which department would you like to remove?',
+                choices: function() {
+                    let choicesArr = [];
+                    for (var i = 0; i < res.length; i++) {
+                        choicesArr.push(res[i].name);
+                    }
+                    return choicesArr;
+                }
+            }
+        ])
+          .then(response => {
+            db.query(`DELETE FROM department WHERE name = '${response.department}'`,
+            (err, res) => {
+                if (err) throw err;
+                console.log(`
+                ================================
+                Department Removed Successfully!
+                ================================
+                `);
+                mainMenu();
+            })
+          })
+    })
+}
+
+function removeManager() {
+    db.query(`SELECT * FROM manager`, (err, res) => {
+        if (err) throw err;
+        return inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'manager',
+                message: 'Which manager would you like to remove?',
+                choices: function() {
+                    let choicesArr = [];
+                    for (var i = 0; i < res.length; i++) {
+                        choicesArr.push(res[i].first_name);
+                    }
+                    return choicesArr;
+                }
+            }
+        ])
+          .then(response => {
+            db.query(`DELETE FROM manager WHERE first_name = '${response.manager}'`,
+            (err, res) => {
+                if (err) throw err;
+                console.log(`
+                =============================
+                Manager Removed Successfully!
+                =============================
+                `);
+                mainMenu();
+            })
+          })
+    })
+}
+
+function removeRole() {
+    db.query(`SELECT * FROM role`, (err, res) => {
+        if (err) throw err;
+        return inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'role',
+                message: 'Which role would you like to remove?',
+                choices: function() {
+                    let choicesArr = [];
+                    for (var i = 0; i < res.length; i++) {
+                        choicesArr.push(res[i].title);
+                    }
+                    return choicesArr;
+                }
+            }
+        ])
+          .then(response => {
+            db.query(`DELETE FROM role WHERE title = '${response.role}'`,
+            (err, res) => {
+                if (err) throw err;
+                console.log(`
+                ==========================
+                Role Removed Successfully!
+                ==========================
+                `);
+                mainMenu();
+            })
+          })
+    })
+}
+
 // The main menu that you'll see in the app
 function mainMenu() {
     // connection to the sql database
@@ -498,6 +729,8 @@ function mainMenu() {
         message: 'What would you llike to do?',
         choices: [
             'View All Employees',
+            'View Employees By Manager',
+            'View Employees By Department',
             'Add Employee',
             'View Departments',
             'Add Department',
@@ -506,6 +739,10 @@ function mainMenu() {
             'View All Roles',
             'Add Role',
             'Update Employee Role',
+            'Remove Employee',
+            'Remove Department',
+            'Remove Manager',
+            'Remove Role',
             'Quit'
         ]
         }
@@ -514,6 +751,11 @@ function mainMenu() {
     .then(response => {
         if (response.menu === 'View All Employees') {
             viewEmployees();
+        }
+        else if (response.menu === 'View Employees By Manager') {
+            viewEmployeesByManager();
+        }else if (response.menu === 'View Employees By Department') {
+            viewEmployeesByDepartment();
         }
         else if (response.menu === 'Add Employee') {
             addEmployee();
@@ -536,8 +778,17 @@ function mainMenu() {
         else if (response.menu === 'Update Employee Role') {
             updateRole();
         }
-        else if (response.menu === 'Add Manager') {
-            addManager();
+        else if (response.menu === 'Remove Employee') {
+            removeEmployee();
+        }
+        else if (response.menu === 'Remove Department') {
+            removeDepartment();
+        }
+        else if (response.menu === 'Remove Manager') {
+            removeManager();
+        }
+        else if (response.menu === 'Remove Role') {
+            removeRole();
         }
         else if (response.menu === 'Quit') {
             console.log(`            
